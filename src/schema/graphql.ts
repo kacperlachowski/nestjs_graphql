3ffba30856784ec623b943843030f81d3c7c59fd
kacperlachowski <kacperlachowsky@gmail.com>
2023-01-26 19:11:58 +0100

    Feat :sparkles: (Table): Add columns & rows to GraphQL schema

--- src/schema/graphql.ts
+++ src/schema/graphql.ts
@@ -12,18 +12,44 @@ export class Table {
     id: number;
     name: string;
     description?: Nullable<string>;
+    columns?: Nullable<Nullable<Column>[]>;
+    rows?: Nullable<Nullable<Row>[]>;
+}
+
+export class Column {
+    id: number;
+    name: string;
+    table: Table;
+}
+
+export class Row {
+    id: number;
+    table: Table;
+    values: string;
 }
 
 export abstract class IQuery {
     abstract table(): Nullable<Nullable<Table>[]> | Promise<Nullable<Nullable<Table>[]>>;
+
+    abstract column(): Nullable<Nullable<Column>[]> | Promise<Nullable<Nullable<Column>[]>>;
+
+    abstract row(): Nullable<Nullable<Row>[]> | Promise<Nullable<Nullable<Row>[]>>;
 }
 
 export abstract class IMutation {
     abstract addTable(name: string, description?: Nullable<string>): Nullable<Table> | Promise<Nullable<Table>>;
+
+    abstract addColumn(name: string, tableId: number): Nullable<Column> | Promise<Nullable<Column>>;
+
+    abstract addRow(values: string, tableId: number): Nullable<Row> | Promise<Nullable<Row>>;
 }
 
 export abstract class ISubscription {
     abstract newTable(): Nullable<Table> | Promise<Nullable<Table>>;
+
+    abstract newColumn(): Nullable<Column> | Promise<Nullable<Column>>;
+
+    abstract newRow(): Nullable<Row> | Promise<Nullable<Row>>;
 }
 
 type Nullable<T> = T | null;
--- src/table/table.resolver.ts
+++ src/table/table.resolver.ts
@@ -1,22 +1,42 @@
 import { Inject } from '@nestjs/common';
 import { Args, Query, Mutation, Resolver, Subscription } from '@nestjs/graphql';
+import { GraphQLError } from 'graphql';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import { PUB_SUB } from 'src/pubsub/pubsub.module';
-import { Table } from 'src/schema/graphql';
+import { Column, Row, Table } from 'src/schema/graphql';
 
 enum SUBSCRIPTION_EVENTS {
   newTable = 'newTable',
+  newColumn = 'newColumn',
+  newRow = 'newRow',
 }
 
 @Resolver()
 export class TableResolver {
   tables: Table[] = [];
+  columns: Column[] = [];
+  rows: Row[] = [];
+
+  getTable(tableId: number): Table | null {
+    const table = this.tables.find((item) => item.id === tableId);
+    return table ?? null;
+  }
 
   constructor(@Inject(PUB_SUB) private readonly pubSub: RedisPubSub) {}
 
   @Query()
   table() {
     return this.tables;
+  }
+
+  @Query()
+  column() {
+    return this.columns;
+  }
+
+  @Query()
+  row() {
+    return this.rows;
   }
 
   @Mutation()
@@ -37,8 +57,73 @@ export class TableResolver {
     return result;
   }
 
+  @Mutation()
+  addColumn(@Args('name') name: string, @Args('tableId') tableId: number) {
+    const result: Partial<Column> = {
+      id: this.columns.length + 1,
+      name,
+    };
+
+    const table = this.getTable(tableId);
+    if (table) {
+      result.id = tableId;
+    } else {
+      throw new GraphQLError('Invalid argument value', {
+        extensions: { code: 'BAD_USER_INPUT' },
+      });
+    }
+
+    this.pubSub.publish(SUBSCRIPTION_EVENTS.newColumn, { newColumn: result });
+
+    return result;
+  }
+
+  @Mutation()
+  addRow(@Args('values') values: string, @Args('tableId') tableId: number) {
+    const result: Partial<Row> = {};
+
+    const table = this.getTable(tableId);
+    if (table) {
+      result.id = tableId;
+    } else {
+      throw new GraphQLError('Invalid argument value', {
+        extensions: { code: 'BAD_USER_INPUT' },
+      });
+    }
+
+    try {
+      const rowValues = JSON.parse(values);
+
+      if (rowValues) {
+        result.values = values;
+      } else {
+        throw new GraphQLError('Invalid argument value', {
+          extensions: { code: 'BAD_USER_INPUT' },
+        });
+      }
+    } catch {
+      throw new GraphQLError('Invalid argument value', {
+        extensions: { code: 'BAD_USER_INPUT' },
+      });
+    }
+
+    this.pubSub.publish(SUBSCRIPTION_EVENTS.newRow, { newRow: result });
+
+    return result;
+  }
+
   @Subscription()
   newTable() {
     return this.pubSub.asyncIterator(SUBSCRIPTION_EVENTS.newTable);
+  }
+
+  @Subscription()
+  newColumn() {
+    return this.pubSub.asyncIterator(SUBSCRIPTION_EVENTS.newColumn);
+  }
+
+  @Subscription()
+  newRow() {
+    return this.pubSub.asyncIterator(SUBSCRIPTION_EVENTS.newRow);
   }
 }
